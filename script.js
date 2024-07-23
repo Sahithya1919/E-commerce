26c036c88a2f2481cbc574ee31b3c8c4be5c1185
Sahithya1919 <bandojusahithya190604@gmail.com>
2024-07-23 14:39:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+/*const cartBtn=document.querySelectorAll(".cart");
+const cartTable=document.getElementsById("cart-table");*/
+/*let productpic=document.getElementsByTagName("img");
+let productname=document.getElementsByTagName("h5");
+let productprice=document.getElementsByTagName("h4")*/
+//let carttable=document.querySelectorAll("cart");
+//let tableData=document.getElementsByTagName("td");
+//let tableRow=document.getElementsByTagName("tr");
+
+
+/*cart[0].addEventListener('click',myfunction);
+
+function myfunction(){
+     tablerow=tablerow.length;
+     tabledata[1]=productpic.src;
+     tabledata[2]=productname;
+     tabledata[3]=productprice;
+     tabledata[5]=productprice;
+
+}*/
+
+/*cartBtn.forEach(btn => {
+    btn.addEventListener("click", addToCart);
+  });
+  
+  function addToCart() {
+    const productImg = this.previousSibling.previousSibling.src;
+    const productname = this.previousSibling.previousSibling.previousSibling.textContent;
+    const productprice = this.previousSibling.previousSibling.previousSibling.previousSibling.textContent;
+  
+    const tableRow = document.createElement("tr");
+     tableData = [
+      document.createElement("td"),
+      document.createElement("td"),
+      document.createElement("td")
+    ];
+  
+    tableData[1].innerHTML = `<img src="${productImg}" alt="">`;
+    tableData[2].innerHTML = productname;
+    tableData[3].innerHTML = productprice;
+    tableData[5].innerHTML = productprice;
+  
+    tableRow.append(...tableData);
+    cartTable.appendChild(tableRow);
+  }*/
+  
